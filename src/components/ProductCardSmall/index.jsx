b356e5b7e2cd14c63dd3473c8ebdc17819e3cd8e
Paul Tang <paul.tang95@gmail.com>
2021-01-11 19:57:45 -0500

    product detail avanzado

--- src/components/ProductCardSmall/index.jsx
+++ src/components/ProductCardSmall/index.jsx
@@ -3,32 +3,20 @@ import React from 'react'
 const ProductCardSmall = (props) => {
   const {name, image, category, price,} = props
   return (
-    <div className="container my-12 mx-auto px-4 md:px-12">
-    <div className="overflow-hidden rounded-lg shadow-lg">
-
-       <a href="/#">
-         <img alt={name} className="block h-72 w-64" src={image}/>
-       </a>
-
-     <div className="flex items-center justify-between leading-tight p-2 md:p-4">
-         <h1 className="text-lg">
-             <a className="no-underline hover:underline text-black" href="/#">
-                 {name}
-             </a>
-         </h1>
-     </div>
-
-     <div className="grid items-center justify-between leading-none p-2 md:p-4">
-         <a className="flex items-center no-underline hover:underline text-black" href="/#">
-             <p className="ml-2 text-sm">{category}</p>
-         </a>
-      
-         <span>S/. {price}</span>
-     </div>
-
-   </div>
+    <div>
+      <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+        <div class="flex items-end justify-end h-56 w-full bg-cover" style={{backgroundImage: `url(${image})`}}>
+          <button class="p-2rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+          </button>
+        </div>
+        <div class="px-5 py-3">
+          <h3 class="text-gray-700 uppercase">{name}</h3>
+          <span class="text-gray-500 mt-2">S/.{price}</span>
+        </div>
+    </div>
     
-  </div>
+   </div>
   )
 }
 
--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,11 +1,57 @@
 import React from 'react'
+import imagenPrueba from '../../assets/images/enterizo_1.jpg'
+import InnerImageZoom from 'react-inner-image-zoom'
+import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
 
 const ProductDetail = () => {
   return (
     <div>
-      
+       <main className="my-8">
+        <div className="flex container mx-auto px-6 justify-center">
+            <div className="md:flex md:items-center">
+              <div className="grid">
+                <img className="w-20 mb-6 mr-10" src={imagenPrueba} alt="imagen de prueba"/>
+                <img className="w-20 mb-6 mr-10" src={imagenPrueba} alt="imagen de prueba"/>
+                <img className="w-20 mb-6 mr-10" src={imagenPrueba} alt="imagen de prueba"/>
+
+              </div>
+                <div className="w-full h-64 md:w-96 lg:h-96 ">
+                 <InnerImageZoom src={imagenPrueba} zoomSrc={imagenPrueba} className="h-auto w-full rounded-md object-cover max-w-lg mx-auto" />
+                </div>
+                <div className="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
+                    <h3 className="text-gray-700 uppercase text-lg">Vestido kriska </h3>
+                    <span className="text-gray-500 mt-3 text-green-700 font-bold">s/.75.00</span>
+                   
+                    <div className="mt-2">
+                        <label className="text-gray-700 text-sm" for="count">Cantidad</label>
+                        <div className="flex items-center mt-1">
+                            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                                <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            </button>
+                            <span className="text-gray-700 text-lg mx-2">20</span>
+                            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                                <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mt-3">
+                        <label className="text-gray-700 text-sm" for="count">Tallas disponibles: "s" y "m"</label>
+                         
+                    </div>
+                    <div className="flex items-center mt-6">
+                        <button className="px-8 py-2 bg-black text-white text-sm font-medium rounded hover:bg-gray-700 focus:outline-none focus:bg-indigo-500">Comprar ahora</button>
+                        <button className="mx-2 text-gray-600 border rounded-md p-2 hover:bg-gray-200 focus:outline-none">
+                      
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </main>
     </div>
   )
 }
 
 export default ProductDetail;
+ 
